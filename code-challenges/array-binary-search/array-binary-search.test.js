f1aa680e56357041adbb0c8f9a9c06ae088a5529
IbrahemSarayrah <ibrahem.sarayrah@gmail.com>
2021-08-21 17:50:22 +0300

    update class-03

--- code-challenges/array-binary-search/array-binary-search.test.js
+++ code-challenges/array-binary-search/array-binary-search.test.js
@@ -8,9 +8,11 @@ describe('test BinarySearch', () => {
     it('return the index of the array’s element that is equal to the value of the search key else -1', () => {
         let test1 = [4, 8, 15, 16, 23, 42]
         let test2 = [-131, -82, 0, 27, 42, 68, 179]
-        let test3 =  [11, 22, 33, 44, 55, 66, 77]
+        let test3 = [11, 22, 33, 44, 55, 66, 77]
+        let test4 = [1, 2, 3, 5, 6, 7]
         expect(BinarySearch(test1, 15)).toStrictEqual(2)
         expect(BinarySearch(test2, 42)).toStrictEqual(4)
         expect(BinarySearch(test3, 90)).toStrictEqual(-1)
+        expect(BinarySearch(test4, 4)).toStrictEqual(-1)
     })
 })
